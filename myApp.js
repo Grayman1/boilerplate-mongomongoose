0d07f7c5ed929ca70608879e1fd8883f4d9433e9
Grayman1 <>
2021-04-05 23:59:58 +0000

    Challenge #1 code updates -- Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
-
+// iNSTALL MONGOOSE
+var mongoose = require('mongoose');
+// SET-UP MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
