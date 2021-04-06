099008776277155c30db30c4cc1d23f83be41cf1
Grayman1 <>
2021-04-06 17:44:43 +0000

    Challenge #10 code: Delete One Document Using model.findByIdAndRemove

--- myApp.js
+++ myApp.js
@@ -151,7 +151,14 @@ const findAndUpdate = (personName, done) => {
 
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({_id: personId}, (err, data) => {
+  if(err) {
+    console.log(err)
+  } else {
+    console.log(data)
+    done(null, data);
+  }
+})  
 };
 
 const removeManyPeople = (done) => {
